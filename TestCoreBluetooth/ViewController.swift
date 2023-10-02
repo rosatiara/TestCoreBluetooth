3e52d811614e8f79b4d000a434426ef41e2cdd9f
rosatiara <rosatiaragaluh@gmail.com>
2023-10-02 09:15:05 +0700

    Initial Commit

---
+++ TestCoreBluetooth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCoreBluetooth
+//
+//  Created by Rosa Tiara Galuh on 02/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
